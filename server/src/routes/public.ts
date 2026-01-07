83c2dc92a470d623c6d35316256d6102b6c283be
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-01-07 19:57:39 +0300

    feat(public): implement form submission with required field validation

--- server/src/routes/public.ts
+++ server/src/routes/public.ts
@@ -2,11 +2,12 @@ import express, { type Request, type Response } from "express";
 import { query } from "../db.js";
 
 const router = express.Router();
-
+//--
 router.get("/forms/:formId", async (req: Request, res: Response) => {
   const { formId } = req.params;
 
   try {
+    // get form
     const formResult = await query(
       "SELECT title, description FROM forms WHERE id = $1",
       [formId]
@@ -15,11 +16,13 @@ router.get("/forms/:formId", async (req: Request, res: Response) => {
       return res.status(404).json({ error: "Form not found" });
     }
 
+    // get questions
     const questionResult = await query(
       "SELECT id, text, type, is_required FROM questions WHERE form_id = $1",
       [formId]
     );
 
+    // get options for every question
     const questionsWithOptions = await Promise.all(
       questionResult.rows.map(async (question: any) => {
         const optionsResult = await query(
@@ -37,6 +40,53 @@ router.get("/forms/:formId", async (req: Request, res: Response) => {
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "A server error occurred" });
+  }
+});
+
+//--
+router.post("/forms/:formId/submit", async (req: Request, res: Response) => {
+  const { formId } = req.params;
+  const { answers } = req.body;
+
+  try {
+    // get questions of form and its necessity situations
+    const questions = await query(
+      "SELECT id, is_required FROM questions WHERE form_id = $1",
+      [formId]
+    );
+
+    // validation
+    for (const q of questions.rows) {
+      const answer = answers.find((a: any) => a.question_id === q.id);
+      if (q.is_required && (!answer || !answer.value)) {
+        return res
+          .status(400)
+          .json({ error: `Question ID ${q.id} is required.` });
+      }
+    }
+    // save the answer to the main table
+    const responseResult = await query(
+      "INSERT INTO responses (form_id) VALUES ($1) RETURNING id",
+      [formId]
+    );
+
+    const responseId = responseResult.rows[0].id;
+
+    // save all answers
+    for (const ans of answers) {
+      await query(
+        "INSERT INTO answers (response_id, question_id, answer_text) VALUES ($1, $2, $3)",
+        [responseId, ans.question_id, ans.value]
+      );
+    }
+
+    //return confirmation message
+    res.status(201).json({ message: "Form submitted successfully" });
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .json({ error: "An error occurred while saving the answer." });
   }
 });
 
