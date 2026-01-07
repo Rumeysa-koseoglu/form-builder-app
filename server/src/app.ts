126291d38e6f575bc77929ba65f29308bad7ca05
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-01-07 15:59:56 +0300

    refactor: modularize app.ts and move routes to seperate files

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,86 +1,20 @@
 import express from "express";
-import type { Request, Response, NextFunction } from "express";
-import { Pool } from "pg";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import authRoutes from "./routes/auth.js";
+import formRoutes from "./routes/forms.js";
+import cors from "cors";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+app.use("/api/auth", authRoutes);
+app.use("/api/forms", formRoutes);
 
-const JWT_SECRET = process.env.JWT_KEY;
-if (!JWT_SECRET) {
-  throw new Error("JWT_SECRET is not defined in environment variables");
-}
-
-// --- MIDDLEWARE: Authentcate Token ---
-export const authenticateToken = (
-  req: any,
-  res: Response,
-  next: NextFunction
-) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-
-  if (!token) return res.status(401).json({ error: "Acces denied" });
-
-  jwt.verify(token, JWT_SECRET, (err: any, user: any) => {
-    if (err) return res.status(403).json({ error: "Invalid token" });
-    req.user = user;
-    next();
-  });
-};
-
-//--- ROUTE: Register ---
-app.post("/register", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  try {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const result = await pool.query(
-      "INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id, email",
-      [email, hashedPassword]
-    );
-    res.status(201).json(result.rows[0]);
-  } catch (err: any) {
-    res.status(400).json({ error: "User already exists or data invalid" });
-  }
-});
-
-// --- ROUTE: Login ---
-app.post("/login", async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  try {
-    const userResult = await pool.query(
-      "SELECT * FROM users WHERE email = $1",
-      [email]
-    );
-    const user = userResult.rows[0];
-    if (!user) return res.status(400).json({ error: "User not found" });
-
-    const validPassword = await bcrypt.compare(password, user.password_hash);
-    if (!validPassword)
-      return res.status(403).json({ error: "Incorrect password" });
-
-    //Generate JWT
-    const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    res.json({ token });
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-//--- PROTECTED ROUTE EXAMPLE ---
-app.get("/me", authenticateToken, (req: any, res: Response) => {
-  res.json({ message: "Welcome to your profile", user: req.user });
+app.use((err: any, req: any, res: any, next: any) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
 });
 
 app.listen(port, () => {
