f07632814f095890807db262942f75d59ed471df
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2026-01-21 23:37:06 +0300

    fix: add proxy settings to

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,4 +12,14 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
+
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
